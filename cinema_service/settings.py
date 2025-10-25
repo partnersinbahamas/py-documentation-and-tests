ea6598c0f6034045424d831a772bc6e271aa0f17
Denys Bokov <denisbokov1703@gmail.com>
2025-10-25 19:46:30 +0200

    fix: update throttle_rates

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -125,10 +125,10 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.AnonRateThrottle',
         'rest_framework.throttling.UserRateThrottle'
     ],
-    # 'DEFAULT_THROTTLE_RATES': {
-    #     'anon': '5/minute',
-    #     'user': '100/minute'
-    # }
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/minute',
+        'user': '30/minute'
+    }
 }
 
 SPECTACULAR_SETTINGS = {
